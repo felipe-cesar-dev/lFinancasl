7d46f925f69cba28986a8cd1d7f976d4a6a85245
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2025-02-17 23:30:51 -0300

    Armazenar Dados

--- financas/app/(home)/index.tsx
+++ financas/app/(home)/index.tsx
@@ -1,23 +1,49 @@
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useUser } from '@clerk/clerk-expo'
 
-export default function Page() {
+const Page = () => {
+  const [texto, setTexto] = useState('');
+  const [valor, setValor] = useState('');
+  const [data, setData] = useState('');
   const { user } = useUser()
 
+  const armazenarDados = async () => {
+    try {
+      const chave = `dados-${Date.now()}`;
+      const dados = { texto, valor, data };
+      await AsyncStorage.setItem(chave, JSON.stringify(dados));
+      setTexto('');
+      setValor('');
+      setData('');
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
+    <View style={{ padding: 50 }}>
+      <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      <TextInput
+        value={texto}
+        onChangeText={(text) => setTexto(text)}
+        placeholder="Digite a manutenção:"
+      />
+      <TextInput
+        value={valor}
+        onChangeText={(text) => setValor(text)}
+        placeholder="Digite um valor:"
+        keyboardType="numeric"
+      />
+      <TextInput
+        value={data}
+        onChangeText={(text) => setData(text)}
+        placeholder="Digite uma data:"
+      />
+      <Button title="Armazenar" onPress={armazenarDados} />
     </View>
-  )
-}
+  );
+};
+
+export default Page;
